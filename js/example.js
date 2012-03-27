749aea50ce034321337ec5f8e7b2e8b6c73d5885
Marcelio Leal <marcelioleal@gmail.com>
2012-03-27 01:29:24 -0300

    refactoring directories

--- js/example.js
+++
@@ -1,7 +0,0 @@
-// Run the script on DOM ready:
-$(function(){
- 	$('#graph').visualize({type: 'pie', height: '300px', width: '420px'});
-	$('#graph').visualize({type: 'bar', width: '420px'});
-	$('#graph').visualize({type: 'area', width: '420px'});
-	$('#graph').visualize({type: 'line', width: '420px'});
-});
